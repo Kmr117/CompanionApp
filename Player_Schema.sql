b07d9bfb0f1706a31fa2df369d82379dfa2529a5
Raghav <raghavw29@gmail.com>
2017-11-27 18:22:02 -0500

    playerschema

---
+++ Player_Schema.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE `gamedatabase` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE TABLE `player` (
+  `Player_ID` int(11) NOT NULL,
+  `PlayerFirstName` varchar(45) NOT NULL,
+  `PlayerLastName` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
