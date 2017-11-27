86fec1ebb5686d7713006cdfcaa9c8388a21ec81
Kaius Reed <kmr117@case.edu>
2017-11-27 18:31:55 -0500

    Updated player schema

--- Player_Schema.sql
+++ Player_Schema.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE `gamedatabase` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `gamedatabase`;
 CREATE TABLE `player` (
   `Player_ID` int(11) NOT NULL,
   `PlayerFirstName` varchar(45) NOT NULL,
