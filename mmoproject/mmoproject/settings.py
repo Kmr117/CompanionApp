7f38897bf73a950ac544fccb7641af812a6ba6e5
Raghav <raghavw29@gmail.com>
2017-12-07 20:14:14 -0500

    updated

--- mmoproject/mmoproject/settings.py
+++ mmoproject/mmoproject/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'gamedatabase',
         'USER': 'root',
-        'PASSWORD': 'gdbpass',
+        'PASSWORD': 'Tarun123',
         'HOST': 'localhost',
         'PORT': '3306',
     }
